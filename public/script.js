0d57fa8753939c1e139692476ff4f542c695a211
nodekage <anthonycuzoho@gmail.com>
2023-03-18 23:33:12 +0100

    RoundUp

--- public/script.js
+++ public/script.js
@@ -1,54 +1,217 @@
-
 // Get DOM elements
 const chatInput = document.querySelector('.chat-input');
 const chatArea = document.querySelector('.chat-messages');
 
 
+// Get sessionId or generate a new one
+const sessionId = getSessionId();
 
-function sendMessage() {
-	// Get user input
-	const message = chatInput.value;
+async function sendMessage() {
+  // Get user input
+  const message = chatInput.value;
 
-	// Create user message element and add to chat area
-  const userMessage = document.createElement("div");
-  userMessage.classList.add("message", "user-message");
-  const textContainer = document.createElement("div");
-  textContainer.classList.add("text-container");
+  // Create user message element and add to chat area
+  const userMessage = document.createElement('div');
+  userMessage.classList.add('message', 'user-message');
+  const textContainer = document.createElement('div');
+  textContainer.classList.add('text-container');
   textContainer.textContent = message;
   userMessage.appendChild(textContainer);
   chatArea.appendChild(userMessage);
+
+  // Generate bot response
+  const botResponse = await generateResponse(message, sessionId);
+
+  // Create bot message element and add to chat area
+  const botMessage = document.createElement('div');
+  botMessage.classList.add('message', 'bot-message');
+  botMessage.textContent = botResponse;
+  chatArea.appendChild(botMessage);
+
+  // Clear user input
+  chatInput.value = '';
+
+  // Scroll to bottom of chat area
+  chatArea.scrollTop = chatArea.scrollHeight;
+}
+
+async function generateResponse(message, sessionId) {
+  return new Promise(async (resolve, reject) => {
+    switch (message) {
+      case '1':
+        const orderMessage = await placeOrder(message, sessionId);
+        resolve(
+          '2. DoubleCheeseBurger : $4.99..  \n' +
+            '3. BreakfastBurrito : $14.99..........\n' +
+            '4. MilkShake : $3.50.......................\n' +
+            '5. French Fries : $4.99..............\n\n' +
+            'Please select an item number to add to your order.',
+          orderMessage
+        );
+        break;
+      case '2':
+        let order2Result = '';
+        order2Result = await order2(message, sessionId);
+        resolve('DoubleCheeseBurger added to cart, press 1 to go back to menu.',
+        order2Result
+        );
+        break;
+      case '3':
+         let order3Result = '';
+         order3Result = await order3(message, sessionId);
+         resolve('BreakfastBurrito added to cart, press 1 to go back to menu.',
+         order3Result
+        );
+        break;
+        case '99':
+        let order99Result = await order99(sessionId);
+         resolve(`${order99Result}`);
+        break;
+      case '98':
+        let order98Result = await order98(sessionId);
+         resolve(`${order98Result}`);
+        break;
+      case '97':
+        let order97Result = await order97(sessionId);
+         resolve(`${order97Result}`);
+        break;
+      case '0':
+        resolve('Order cancelled!'); // add logic to handle cancelling orders
+        break;
+      default:
+        resolve("I'm sorry, I didn't understand that. Please select one of the options.");
+        break;
+    }
+  });
+}
+
+// PLACE ORDER FUNCTION
+
+async function placeOrder(message, sessionId) {
+  const response = await fetch('http://localhost:5500/api/chatbot/order', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'sessionId': sessionId, // Include session ID in headers
+    },
+    body: JSON.stringify({ message: message }),
+    credentials: 'include', // Include cookies in the request
+  });
+  const data = await response.json();
+
+  console.log(data);
+}
+
+
+// ORDER 2 FUNCTION 'DoubleCheeseBurger : $4.99'
+
+async function order2(message, sessionId) {
+    const response = await fetch('http://localhost:5500/api/chatbot/order2', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sessionId': sessionId, // Include session ID in headers
+      },
+      body: JSON.stringify({ message: "DoubleCheeseBurger : $4.99" }),
+      credentials: 'include', // Include cookies in the request
+    });
+    const data = await response.json();
+  
+    console.log(data);
+  }
+
+
+
+
+// ORDER 3 FUNCTION 'DoubleCheeseBurger : $4.99'
+
+async function order3(message, sessionId) {
+    const response = await fetch('http://localhost:5500/api/chatbot/order3', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sessionId': sessionId, // Include session ID in headers
+      },
+      body: JSON.stringify({ message: "BreakfastBurrito : $14.99" }),
+      credentials: 'include', // Include cookies in the request
+    });
+    const data = await response.json();
+  
+    console.log(data);
+  }
+
+  
+// ORDER 99 FUNCTION 'PLACE ORDER'
+
+async function order99(sessionId) {
+    const response = await fetch('http://localhost:5500/api/chatbot/order99', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sessionId': sessionId, // Include session ID in headers
+      },
+      body: JSON.stringify({ message: "99" }),
+      credentials: 'include', // Include cookies in the request
+    });
+    const data = await response.json();
+    return data;
+  }
   
 
-	// Generate bot response
-	const botResponse = generateResponse(message);
+  
+// ORDER 98 FUNCTION 'ORDER HISTORY'
 
-	// Create bot message element and add to chat area
-	const botMessage = document.createElement("div");
-	botMessage.classList.add("message", "bot-message");
-	botMessage.textContent = botResponse;
-	chatArea.appendChild(botMessage);
+async function order98(message, sessionId) {
+    const response = await fetch('http://localhost:5500/api/chatbot/order98', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sessionId': sessionId, // Include session ID in headers
+      },
+      body: JSON.stringify({ message: "BreakfastBurrito : $14.99" }),
+      credentials: 'include', // Include cookies in the request
+    });
+    const data = await response.json();
+    return data;
+  
+  }
 
-	// Clear user input
-	chatInput.value = "";
 
-	// Scroll to bottom of chat area
-	 chatArea.scrollTop = chatArea.scrollHeight;
-}
+// ORDER 97 FUNCTION 'SEE ORDER'
 
-function generateResponse(message) {
-	// Check user input and generate response
-	switch (message) {
-		case "1":
-			return "Here is a list of items from our restaurant: ..."; // add your list of items here
-		case "99":
-			return "Order placed!"; // add logic to handle placing orders
-		case "98":
-			return "Here is your order history: ..."; // add logic to retrieve order history
-		case "97":
-			return "Here is your current order: ..."; // add logic to retrieve current order
-		case "0":
-			return "Order cancelled!"; // add logic to handle cancelling orders
-		default:
-			return "I'm sorry, I didn't understand that. Please select one of the options.";
-	}
-}
+async function order97(message, sessionId) {
+    const response = await fetch('http://localhost:5500/api/chatbot/order97', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'sessionId': sessionId, // Include session ID in headers
+      },
+      body: JSON.stringify({ message: "BreakfastBurrito : $14.99" }),
+      credentials: 'include', // Include cookies in the request
+    });
+    const data = await response.json();
+  
+    console.log(data);
+  }
+
+
+
+
+
+
+
+
+
+
+
+function getSessionId() {
+    let sessionId = localStorage.getItem('sessionId');
+    if (!sessionId) {
+      sessionId = "0123456789"; // generate a new session ID if it doesn't exist
+      localStorage.setItem('sessionId', sessionId); // store the new session ID in local storage
+    }
+    return sessionId;
+    
+  }
+  
+ 
--- server.js
+++ server.js
@@ -1,16 +1,231 @@
-const express = require('express')
-require ('dotenv').config()
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
-const app = express()
 
-const PORT = process.env.PORT
+require('dotenv').config();
 
+const app = express();
+
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500',
+    credentials: true,
+  };
+  
+  app.use(cors(corsOptions));
+
+
+
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+
+
+const MONGO_URL = process.env.MONGODB_URL;
+const PORT = process.env.PORT || 5500;
+
+const chatSchema = new mongoose.Schema({
+    message: String,
+    timestamp: Date
+  });
+ 
+  const orderSchema = new mongoose.Schema({
+    message: String,
+  });
+  
+
+const sessionSchema = new mongoose.Schema({
+    _id: String,
+    chats: [chatSchema],
+    orders: [orderSchema]
+  });
+  
+
+const Session = mongoose.model('Session', sessionSchema);
+
+mongoose.connect(MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Database'));
 
 app.get('/', (req, res) => {
-    res.send({ message: 'Welcome To The Blog!!!!'})
-})
+  res.send({ message: 'Welcome to the Chow MATe Blog!!!!' });
+});
+
+
+
+
+
+
+
+// PLACE ORDER FUNCTION
+
+app.post('/api/chatbot/order', async (req, res) => {
+    let sessionId = req.cookies.sessionId || req.headers.sessionid || uuid.v4();
+    
+    const message = JSON.stringify(req.body.message);
+    const timestamp = new Date();
+    
+    let session = await Session.findById(sessionId);
+    if (!session) {
+      session = new Session({
+        _id: sessionId,
+        chats: [],
+      }); 
+    }
+    
+    session.chats.push({ message, timestamp });
+    await session.save();
+    
+    // Set the cookie before sending the response
+    res.cookie('sessionId', sessionId, {
+        maxAge: 86400000,
+        path: '/'
+    });
+    
+    // Return a response to the client
+    res.json({ message: 'Success', botResponse: 'This is a dummy response from the server' });
+  });
+
+
+
+
+
+
+
+// ORDER 2 FUNCTION 'DoubleCheeseBurger : $4.99'
+
+app.post('/api/chatbot/order2', async (req, res) => {
+    let sessionId = req.cookies.sessionId || req.headers.sessionid || uuid.v4();
+    
+    const message = JSON.stringify(req.body.message);
+    const timestamp = new Date();
+    
+    let session = await Session.findById(sessionId);
+    if (!session) {
+      session = new Session({
+        _id: sessionId,
+        orders: [],
+      }); 
+    }
+    
+    session.orders.push({ message });
+    await session.save();
+    
+    // Set the cookie before sending the response
+    res.cookie('sessionId', sessionId, {
+        maxAge: 86400000,
+        path: '/'
+    });
+    
+    // Return a response to the client
+    res.json("Order placed!");
+  });
+
+
+
+// ORDER 3 FUNCTION 'DoubleCheeseBurger : $4.99'
+
+app.post('/api/chatbot/order3', async (req, res) => {
+    let sessionId = req.cookies.sessionId || req.headers.sessionid || uuid.v4();
+    
+    const message = JSON.stringify(req.body.message);
+    const timestamp = new Date();
+    
+    let session = await Session.findById(sessionId);
+    if (!session) {
+      session = new Session({
+        _id: sessionId,
+        orders: [],
+      }); 
+    }
+    
+    session.orders.push({ message });
+    await session.save();
+    
+    // Set the cookie before sending the response
+    res.cookie('sessionId', sessionId, {
+        maxAge: 86400000,
+        path: '/'
+    });
+    
+    // Return a response to the client
+    res.json("Order 3 placed!");
+  });  
+
+
+
+// ORDER 99 FUNCTION 'PLACE ORDER'
+
+app.post('/api/chatbot/order99', async (req, res) => {
+    let sessionId = req.cookies.sessionId || req.headers.sessionid || uuid.v4();
+    
+  //  const message = JSON.stringify(req.body.message);
+   // const timestamp = new Date();
+    
+   let session = await Session.findById(sessionId);
+   if (!session) {
+     res.json("No order to place")
+     return
+   }
+   
+   if (session.orders.length < 1) {
+     res.json("No order to place")
+     return
+   }
+   
+
+   // Set the cookie before sending the response
+   res.cookie('sessionId', sessionId, {
+     maxAge: 86400000,
+     path: '/'
+   });
+   
+   // Return a response to the client
+   if (session.orders.length >= 1) {
+    res.json("Order Placed")
+  }
+   
+  });    
+    
+
+// ORDER 98 FUNCTION 'ORDER HISTORY'
+
+app.post('/api/chatbot/order98', async (req, res) => {
+    let sessionId = req.cookies.sessionId || req.headers.sessionid || uuid.v4();
+  
+    let session = await Session.findById(sessionId);
+    console.log(session)
+    if (!session) {
+      res.json("No order history.")
+      return
+    }
+  
+    if (session.orders.length < 1) {
+      res.json("No order history")
+      return
+    }
+  
+    // Set the cookie before sending the response
+    res.cookie('sessionId', sessionId, {
+      maxAge: 86400000,
+      path: '/'
+    });
+  
+    // Return a response to the client
+    console.log(session.orders)
+    res.json(session.orders);
+  });
+   
+  
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
